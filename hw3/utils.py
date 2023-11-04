dc815d53397310c3cde0b75a7ab062a5f4b90442
saahil-jain <saahiljain98@gmail.com>
2023-11-03 20:33:08 -0400

    hw3

--- hw3/utils.py
+++ hw3/utils.py
@@ -26,12 +26,180 @@ def example_transform(example):
 # For typos, you can try to simulate nearest keys on the QWERTY keyboard for some of the letter (e.g. vowels)
 # You can randomly select each word with some fixed probability, and replace random letters in that word with one of the
 # nearest keys on the keyboard. You can vary the random probablity or which letters to use to achieve the desired accuracy.
+def switch_letters(word):
+    if len(word) < 2 or random.random() > 0.1:
+        return word
 
+    positions = random.sample(range(len(word)), 2)
+
+    # Switch the letters at the selected positions
+    word_list = list(word)
+    word_list[positions[0]], word_list[positions[1]] = word_list[positions[1]], word_list[positions[0]]
+    return ''.join(word_list)
+
+
+def parse_and_switch(sentence):
+    words = sentence.split()
+    switched_words = [switch_letters(word) for word in words]
+    return ' '.join(switched_words)
+
+def generate_typing_errors(sentence):
+    sentence = parse_and_switch(sentence)
+    alternate_characters = {
+        'q': ['w'], 
+        'w': ['q', 'e'], 
+        'e': ['w', 'r'], 
+        'r': ['e', 't'], 
+        't': ['r', 'y'], 
+        'y': ['t', 'u'], 
+        'u': ['y', 'i'], 
+        'i': ['u', 'o'], 
+        'o': ['i', 'p'], 
+        'p': ['o', '['], 
+        'a': ['', 's'], 
+        's': ['a', 'd'], 
+        'd': ['s', 'f'], 
+        'f': ['d', 'g'], 
+        'g': ['f', 'h'], 
+        'h': ['g', 'j'], 
+        'j': ['h', 'k'], 
+        'k': ['j', 'l'], 
+        'l': ['k', ';'], 
+        'z': ['x'], 
+        'x': ['z', 'c'], 
+        'c': ['x', 'v'], 
+        'v': ['c', 'b'], 
+        'b': ['v', 'n'], 
+        'n': ['b', 'm'], 
+        'm': ['n', ','], 
+        ' ': ['']
+    }
+    editable_characters = set(alternate_characters.keys())
+
+    transformed_sentence = sentence
+
+    for i, letter in enumerate(sentence):
+        if letter in editable_characters and random.random() <= 0.01:
+            replacement_letter = random.choice(alternate_characters[letter])
+            transformed_sentence = transformed_sentence[:i] + replacement_letter + transformed_sentence[i + 1:]
+
+    return transformed_sentence
 
 ### Rough guidelines --- synonym replacement
 # For synonyms, use can rely on wordnet (already imported here). Wordnet (https://www.nltk.org/howto/wordnet.html) includes
 # something called synsets (which stands for synonymous words) and for each of them, lemmas() should give you a possible synonym word.
 # You can randomly select each word with some fixed probability to replace by a synonym.
+def get_synonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.append(lemma.name())
+    return synonyms
+
+def generate_synonyms(sentence):
+    words = sentence.split()
+    new_words = words
+
+    max_replacements = 3
+    replacements_complete = 0
+
+    for i, word in enumerate(words):
+
+        replacement = word
+        if replacements_complete < max_replacements and len(word) > 2:
+            if random.random() <= 0.2:
+                synonyms = get_synonyms(word)
+                if len(synonyms)>0:
+                    replacement = random.choice(synonyms)
+                    replacements_made += 1
+        new_words[i] = replacement
+
+    return " ".join(new_words)
+
+def switch_US_and_UK_english(sentence):
+    if random.random() <= 0.2:
+        return sentence
+    
+    spelling_replacements = {
+        'color': 'colour',
+        'colour': 'color',
+        'favorite': 'favourite',
+        'favourite': 'favorite',
+        'neighbor': 'neighbour',
+        'neighbour': 'neighbor',
+        'honor': 'honour',
+        'honour': 'honor',
+        'humor': 'humour',
+        'humour': 'humor',
+        'calibre': 'caliber',
+        'caliber': 'calibre',
+        'centre': 'center',
+        'center': 'centre',
+        'fibre': 'fiber',
+        'fiber': 'fibre',
+        'goitre': 'goiter',
+        'goiter': 'goitre',
+        'litre': 'liter',
+        'liter': 'litre',
+        'lustre': 'luster',
+        'luster': 'lustre',
+        'manoeuvre': 'maneuver',
+        'maneuver': 'manoeuvre',
+        'meagre': 'meager',
+        'meager': 'meagre',
+        'metre': 'meter',
+        'meter': 'metre',
+        'mitre': 'miter',
+        'miter': 'mitre',
+        'nitre': 'niter',
+        'niter': 'nitre',
+        'ochre': 'ocher',
+        'ocher': 'ochre',
+        'reconnoitre': 'reconnoiter',
+        'reconnoiter': 'reconnoitre',
+        'sabre': 'saber',
+        'saber': 'sabre',
+        'saltpetre': 'saltpeter',
+        'saltpeter': 'saltpetre',
+        'sepulchre': 'sepulcher',
+        'sepulcher': 'sepulchre',
+        'sombre': 'somber',
+        'somber': 'sombre',
+        'spectre': 'specter',
+        'specter': 'spectre',
+        'theatre': 'theater',
+        'theater': 'theatre',
+        'titre': 'titer',
+        'titer': 'titre'
+    }
+    word_ending_replacement = {
+        'yze': 'yse',
+        'yse': 'yze',
+        'yzed': 'ysed',
+        'ysed': 'yzed',
+        'ize': 'ise',
+        'ise': 'ize',
+        'izer': 'iser',
+        'iser': 'izer',
+        'ization': 'isation',
+        'isation': 'ization',
+        'logue': 'log',
+        'log': 'logue',
+    }
+    spelling_replacement_keys = set(spelling_replacements.keys())
+    word_ending_keys = set(word_ending_replacement.keys())
+    
+    words = sentence.split()
+    new_words = words
+    for i, word in enumerate(words):
+        if word in spelling_replacement_keys:
+            new_words[i] = spelling_replacements[word]
+        else:
+            for ending in word_ending_keys:
+                if word.endswith(ending):
+                    new_words[i] = word[:-len(ending)] + word_ending_replacement[ending]
+
+    return " ".join(new_words)
 
 
 def custom_transform(example):
@@ -43,9 +211,27 @@ def custom_transform(example):
     # how you could implement two of them --- synonym replacement and typos.
 
     # You should update example["text"] using your transformation
+    sentences = example["text"].split(". ")
+    new_sentences = sentences
 
-    raise NotImplementedError
+    for i, sentence in enumerate(sentences):
+        new_sentence = generate_synonyms(sentence)
+        new_sentence = generate_typing_errors(new_sentence)
+        new_sentence = generate_switched_letters(new_sentence)
+        new_sentence = switch_US_and_UK_english(new_sentence)
+        new_sentences.append(new_sentence)
+
+    example["text"] = ". ".join(new_sentences)
+    ##### YOUR CODE ENDS HERE ######
+
+    return example
 
     ##### YOUR CODE ENDS HERE ######
 
     return example
+
+# Example usage
+american_sentence = {}
+american_sentence['text'] = "I love the color of your car, it's my favorite. Let's organize a theater event."
+british_sentence = custom_transform(american_sentence)['text']
+print(british_sentence)
